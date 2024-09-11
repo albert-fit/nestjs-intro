064d3040e09ceb9cf469a1067c427a243606a213
Albert Fit <af@albert-fit.com>
2024-09-11 09:36:20 +0100

    created get and post routes for the users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/:id/:optional?') // you can use any keyword for optional as long as you place a ? at the end.
+  public getUsers(@Param() params: any, @Query() query: any) {
+    const id: number = params.id;
+    const optional: string = params.optional;
+    return `You sent a get request to users endpoint with id: ${id},
+    with an optional parameter of ${optional},
+    and the query parameter is limit: ${query.limit} and offset: ${query.offset}`;
+  }
+
+  @Post()
+  public createUsers(@Body() body: any) {
+    console.log(body);
+    return 'You sent a post request to users endpoint';
+  }
+}
