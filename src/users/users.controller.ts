f69301c590564cef574686d3553e45e2a6f19360
Albert Fit <af@albert-fit.com>
2024-09-11 09:43:59 +0100

    used the headers and ip decorators in the controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,22 +8,33 @@ import {
   Param,
   Query,
   Body,
+  Headers,
+  Ip,
 } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id/:optional?') // you can use any keyword for optional as long as you place a ? at the end.
-  public getUsers(@Param() params: any, @Query() query: any) {
-    const id: number = params.id;
-    const optional: string = params.optional;
-    return `You sent a get request to users endpoint with id: ${id},
-    with an optional parameter of ${optional},
-    and the query parameter is limit: ${query.limit} and offset: ${query.offset}`;
+  public getUsers(
+    @Param('id') id: any,
+    params: any,
+    @Query('limit') limit: any,
+    @Query('offset') offset: any,
+  ) {
+    console.log(limit);
+    console.log(offset);
+    return `You sent a get request to users endpoint with id: ${id}`;
   }
 
   @Post()
-  public createUsers(@Body() body: any) {
+  public createUsers(
+    @Body() body: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(headers);
     console.log(body);
+    console.log(ip);
     return 'You sent a post request to users endpoint';
   }
 }
